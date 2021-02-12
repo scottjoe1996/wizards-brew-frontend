b49882af2cc7ee35112d37c9a7da82d7c5f2c7a1
scottjoe1996 <scottjoe1996@yahoo.co.uk>
2021-02-12 16:36:51 +0000

    removed app.spec.tsx

--- src/app.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './app';
-
-it('should render', () => {
-  const { getByText } = render(<App />);
-  getByText('Test Page');
-});
