18bb738f45a24db004c236ad2958e87040e5222e
scottjoe1996 <scottjoe1996@yahoo.co.uk>
2021-02-12 17:14:04 +0000

    added icon to header and lint to circle ci build

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,24 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, createStyles, makeStyles, Theme, Toolbar, Typography } from '@material-ui/core';
+import { LocalLibraryRounded } from '@material-ui/icons';
 import React from 'react';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    menuButton: {
+      marginRight: theme.spacing(2),
+      marginBottom: theme.spacing(0.25)
+    }
+  })
+);
+
 const Header: React.FunctionComponent = () => {
+  const meme;
+  const classes = useStyles();
+
   return (
     <AppBar position='sticky'>
       <Toolbar>
+        <LocalLibraryRounded className={classes.menuButton} />
         <Typography variant='h6'>Wizards Brew</Typography>
       </Toolbar>
     </AppBar>
