3e990645110369e549fab97a25eac604c2fce07d
scottjoe1996 <scottjoe1996@yahoo.co.uk>
2021-09-11 15:28:29 +0100

    Implemented basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container, createMuiTheme, createStyles, makeStyles, MuiThemeProvider } from '@material-ui/core';
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './components/header/header';
 import Spellbook from './pages/spellbook/spellbook';
@@ -27,12 +28,18 @@ const useStyles = makeStyles(() =>
 const App: React.FunctionComponent = () => {
   const classes = useStyles();
   return (
-    <MuiThemeProvider theme={customTheme}>
-      <Header />
-      <Container maxWidth='xl' className={classes.container}>
-        <Spellbook />
-      </Container>
-    </MuiThemeProvider>
+    <BrowserRouter>
+      <MuiThemeProvider theme={customTheme}>
+        <Header />
+        <Container maxWidth='xl' className={classes.container}>
+          <Switch>
+            <Route path='/'>
+              <Spellbook />
+            </Route>
+          </Switch>
+        </Container>
+      </MuiThemeProvider>
+    </BrowserRouter>
   );
 };
 
